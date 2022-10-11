b5f08bfc3d6585bced4df7ec12f04a1dcdc042d1
semvdh <semvanderheijden03@gmail.com>
2022-10-11 21:13:01 +0200

    updated nav

--- components/Nav.js
+++ components/Nav.js
@@ -9,7 +9,7 @@ export default function Nav(){
     return (
         <nav className="flex justify-between items-center py-10">
             <Link href="/">
-                <button className="text-2xl font-medium">The Crazy Family 😄</button>
+                <button className="text-xl font-medium">The Crazy Family 😄</button>
             </Link>
             <ul className="flex items-center gap-10">
                 {!user && (
